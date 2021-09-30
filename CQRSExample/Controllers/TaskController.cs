d873c4d51943e7ccdd5c63b3e71991c677736fdc
Berihun Hadis <berihunhadis@gmail.com>
2021-09-30 10:24:45 +0300

    inital commit

---
+++ CQRSExample/Controllers/TaskController.cs
@@ -0,0 +1,31 @@
+﻿using CQRSExample.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSExample.Controllers
+{
+   
+        [ApiController]
+        public class TaskController : ControllerBase
+        {
+            private readonly IMediator _mediatr;
+            public TaskController(IMediator mediatr)
+            {
+                _mediatr = mediatr;
+            }
+            [HttpGet("/{Id}")]
+            public async Task<IActionResult> GetDatabyId(int Id)
+            {
+                var result = await _mediatr.Send(new GetData.Query(Id));
+                return result != null ? Ok(result) : NotFound();
+            }
+            [HttpPost("")]
+            public async Task<IActionResult> GetDatabyId(AddData.Command command) => Ok(await _mediatr.Send(command));
+        }
+    
+}
